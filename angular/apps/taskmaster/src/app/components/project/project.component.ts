83924b00d61e6bbbf8452fae34eaabc92e4e6e9e
Andre Davcev <andre.davcev@gmail.com>
2025-04-24 00:13:03 -0500

    Change to use application.yml

--- angular/apps/taskmaster/src/app/components/project/project.component.ts
+++ angular/apps/taskmaster/src/app/components/project/project.component.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { ProjectTitleComponent } from './project-title/project-title.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ProjectTitleComponent,
+    TaskListComponent,
+    ProgressBarComponent,
+  ],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
