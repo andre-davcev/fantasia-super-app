502181fab9d7fe392eb9e3406758e108a6b0721d
Andre Davcev <andre.davcev@gmail.com>
2025-05-01 13:42:52 -0400

    Add updates to form

---
+++ front-end/apps/taskmaster-angular/src/app/components/project/task-form/enums/task-form-submit.enum.ts
@@ -0,0 +1,4 @@
+export enum TaskFormSubmit {
+  Submit = 'submit',
+  Cancel = 'cancel',
+}
--- front-end/apps/taskmaster-angular/src/app/components/project/task-list/task-list.component.ts
+++ front-end/apps/taskmaster-angular/src/app/components/project/task-list/task-list.component.ts
@@ -3,7 +3,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { Task } from '../../../models';
 import { TaskService } from '../../../services';
+import { FormType } from '../shared/enums';
 import { TaskFormComponent } from '../task-form/task-form.component';
+
+const TASK_EMPTY: Task = {
+  name: '',
+  description: '',
+  completed: false,
+  project: 0,
+  id: 0,
+};
 
 @Component({
   selector: 'app-task-list',
@@ -16,6 +25,8 @@ import { TaskFormComponent } from '../task-form/task-form.component';
 export class TaskListComponent {
   public tasks: Array<Task> = [];
   public showModal: boolean = false;
+  public taskSelected: Task = TASK_EMPTY;
+  public formType: FormType = FormType.Create;
 
   constructor(private taskService: TaskService) {
     this.tasks = this.taskService.getAll();
@@ -31,6 +42,12 @@ export class TaskListComponent {
     updated.completed = !updated.completed;
 
     this.tasks = this.taskService.update(updated);
+  }
+
+  public updateTask(task: Task) {
+    this.taskSelected = task;
+    this.formType = FormType.Update;
+    this.showModal = true;
   }
 
   public deleteTask(id: number): void {
