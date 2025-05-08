446f193d08c560df635c0303567dc7d86b051131
Andre Davcev <andre.davcev@gmail.com>
2025-05-08 17:04:17 -0400

    Add interceptors

--- front-end/apps/taskmaster-angular/src/app/app.config.ts
+++ front-end/apps/taskmaster-angular/src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 import { appRoutes } from './app.routes';
+import { tokenInterceptor } from './interceptors';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideHttpClient()],
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+  ],
 };
