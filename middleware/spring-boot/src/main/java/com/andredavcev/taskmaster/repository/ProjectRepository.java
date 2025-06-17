f8c1ffcf0d67d4b44ef91ace55b48fc500f266ba
Andre Davcev <andre.davcev@gmail.com>
2025-06-17 17:58:13 -0400

    Remove shadow Jar

--- middleware/spring-boot/src/main/java/com/andredavcev/taskmaster/repository/ProjectRepository.java
+++ middleware/spring-boot/src/main/java/com/andredavcev/taskmaster/repository/ProjectRepository.java
@@ -2,6 +2,8 @@ package com.andredavcev.taskmaster.repository;
 
 import com.andredavcev.taskmaster.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
