7eecd53cb7e8975e926dbf4084cd9e4817ab0fee
Andre Davcev <andre.davcev@gmail.com>
2025-09-23 00:32:27 -0400

    Fix e2e

--- apps/taskmaster-angular-e2e/src/e2e/app.cy.ts
+++ apps/taskmaster-angular-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,10 @@
-import { getGreeting } from '../support/app.po';
+import { getTitle } from '../support/app.po';
 
 describe('taskmaster-angular-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
+  it('should display title', () => {
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+    getTitle().contains(/Taskmaster/);
   });
 });
